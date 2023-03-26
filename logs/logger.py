e869eb63b35772bf0970123b4542492411cf5c91
startian <473151579@qq.com>
2023-03-26 21:24:37 +0800

    优化输出

---
+++ logs/logger.py
@@ -0,0 +1,43 @@
+class log(object):
+
+    @staticmethod
+    def show(message):
+        print(message)
+
+    @staticmethod
+    def info(message):
+        print ("\033[94m[I]\033[0m INFO: " + message)
+
+    @staticmethod
+    def err(message):
+        print ("\033[91m[X]\033[0m ERROR: " + message)
+
+    @staticmethod
+    def success(message):
+        print ("\033[92m[+]\033[0m SUCCESS: " + message)
+
+    @staticmethod
+    def warn(message):
+        print ("\033[93m[!]\033[0m WARNING: " + message)
+
+# class log(object):
+
+#     @staticmethod
+#     def show(message):
+#         print message
+
+#     @staticmethod
+#     def info(message):
+#         print "\033[94m[I]\033[0m INFO: " + message
+
+#     @staticmethod
+#     def err(message):
+#         print "\033[91m[X]\033[0m ERROR: " + message
+
+#     @staticmethod
+#     def success(message):
+#         print "\033[92m[+]\033[0m SUCCESS: " + message
+
+#     @staticmethod
+#     def warn(message):
+#         print "\033[93m[!]\033[0m WARNING: " + message
