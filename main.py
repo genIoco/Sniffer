05653a4e327488dd968b6d5eddc52bb00481daf1
startian <473151579@qq.com>
2023-03-27 11:00:40 +0800

    获取所有网卡接口信息

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow
 from scapy.all import *
+from scapy.arch.windows import get_windows_if_list
 import ui.Ui_ui as mainWindow
 
 
@@ -17,7 +18,31 @@ def Ui():
     window = MainWindow()
 
     window.show()
-    app.exec_()
+    app.exec()
+
+
+class Ifaces:
+    '网卡基本信息类'
+
+    def __init__(self) -> None:
+        self.ifaces = conf.ifaces.data
+        # self.ifaces = get_windows_if_list()
+
+    # 打印网卡信息
+    def ShowIfaces(self):
+        for iface_name in self.ifaces:
+            iface = self.ifaces[iface_name]
+            print("name: ", iface.name)
+            print("description: ", iface.description)
+            print("index: ", iface.index)
+            print("mac: ", iface.mac)
+            for ip in iface.ips[4]:
+                print("IPv4: ", ip)
+            for ip in iface.ips[6]:
+                print("IPv6: ", ip)
+
 
 if __name__ == "__main__":
-    Ui()
+    # Ui()
+    ifaces = Ifaces()
+    ifaces.ShowIfaces()
