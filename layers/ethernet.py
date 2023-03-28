064a293d43a50bf52e710e554c4faac3a25faab2
startian <473151579@qq.com>
2023-03-28 10:48:46 +0800

    添加以太网数据帧解析功能

---
+++ layers/ethernet.py
@@ -0,0 +1,38 @@
+from layers.enums import EthernetType
+from layers.layer import Layer
+from packet import Packet
+from util.util import GetBits
+
+
+class Ethernet(Layer):
+    '以太网帧类结构'
+    name = "Ethernet"
+    srcMAC = ""
+    dstMAC = ""
+    ethernetType = None
+    length = 0
+    padding = bytes
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def DecodeFromBytes(self, data: bytes):
+        """以太网数据包解码器"""
+        self.srcMAC = data[0:6].hex(':')
+        self.dstMAC = data[6:12].hex(':')
+        self.ethernetType = GetBits(data[12:14],0,16)
+        self.length = len(data)
+        self.header = data[:14]
+        self.payload = data[14:]
+
+    @property
+    def NextLayerType(self) -> str:
+        return EthernetType(self.ethernetType).name
+
+
+def DecodeEthernet(data: bytes, packet: Packet):
+    eth = Ethernet()
+    eth.DecodeFromBytes(data)
+    packet.AddLayer(eth)
+    packet.SetLinkLayer(eth)
+    packet.NextDecoder(eth.NextLayerType)
