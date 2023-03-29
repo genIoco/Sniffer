bd415f21bbebf6f44b63459619cc878c7675949a
startian <473151579@qq.com>
2023-03-29 09:03:53 +0800

    更新打印日志模块

--- logs/logger.py
+++ logs/logger.py
@@ -1,24 +1,24 @@
 class log(object):
 
     @staticmethod
-    def show(message):
-        print(message)
+    def show(*message):
+        print(*message)
 
     @staticmethod
-    def info(message):
-        print ("\033[94m[I]\033[0m INFO: " + message)
+    def info(*message):
+        print ("\033[94m[I]\033[0m INFO: ",*message)
 
     @staticmethod
-    def err(message):
-        print ("\033[91m[X]\033[0m ERROR: " + message)
+    def err(*message):
+        print ("\033[91m[X]\033[0m ERROR: ",*message)
 
     @staticmethod
-    def success(message):
-        print ("\033[92m[+]\033[0m SUCCESS: " + message)
+    def success(*message):
+        print ("\033[92m[+]\033[0m SUCCESS: " ,*message)
 
     @staticmethod
-    def warn(message):
-        print ("\033[93m[!]\033[0m WARNING: " + message)
+    def warn(*message):
+        print ("\033[93m[!]\033[0m WARNING: ",*message)
 
 # class log(object):
 
